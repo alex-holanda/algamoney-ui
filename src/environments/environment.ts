acaa6aedd04562cd56d48c8f37d6e83d3736c2e9
Alex Holanda <alex.holanda@outlook.com>
2019-07-12 14:28:30 -0300

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,12 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
 
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://algamoney-curso.herokuapp.com',
 
-  tokenWhitelistedDomains: [new RegExp('localhost:8080') ],
-  tokenBlacklistedRoutes: [new RegExp('\/oauth\/token/') ]
+  tokenWhitelistedDomains: [ new RegExp('algamoney-curso.herokuapp.com') ],
+  tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token/') ]
 };
 
 /*
