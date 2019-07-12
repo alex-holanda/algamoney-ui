349c4051d435b196c517d48fb1788d74d6736ac1
Alex Holanda <alex.holanda@outlook.com>
2019-07-12 14:23:37 -0300

    atualizado server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const path = require('path');
 const express = require('express');
+const appName = 'algamoney-ui';
 const app = express();
 
-app.use(express.static(__dirname + '/dist/algamoney-ui'));
+app.use(express.static(`${__dirname}/dist/${appName}`));
 
 app.get('/*', function(req, res) {
-  res.sendFile(__dirname + '/dist/algamoney-ui/index.html');
+  res.sendFile(path.join(`${__dirname}/dist/${appName}/index.html`));
 });
 
 app.listen(process.env.PORT || 4200);
