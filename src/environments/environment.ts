4f7f5f8cb36e456f52d7ca6d9c348bac2f3db0b5
Alex Holanda <alex.holanda@outlook.com>
2019-09-04 14:10:04 -0300

    Configurado env para usar a api heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+//  production: false,
 
-  apiUrl: 'http://localhost:8080',
+//  apiUrl: 'http://localhost:8080',
 
-  tokenWhitelistedDomains: [ new RegExp('localhost:8080') ],
+//  tokenWhitelistedDomains: [ new RegExp('localhost:8080') ],
+//  tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token/') ]
+
+  production: true,
+
+  apiUrl: 'https://algamoney-curso.herokuapp.com',
+
+  tokenWhitelistedDomains: [ new RegExp('algamoney-curso.herokuapp.com') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token/') ]
 };
 
